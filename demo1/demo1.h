207f9f0eaf14e78a55f3706fb2aa1792d5a973af
lzml <202489427@qq.com>
2022-12-09 12:52:04 +0800

    a

---
+++ demo1/demo1.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "resource.h"
+
+
+#include "SkBitmap.h"
+#include "SkPaint.h"
+#include "SkPath.h"
+#include "SkCanvas.h"
+#include "SkDiscretePathEffect.h"
+#include "SkSurface.h"
+#include "GrSurface.h"
+
+
+#pragma comment(lib, "..\\bin\\skiad.lib")
+#pragma comment(lib, "Usp10.lib")
+#pragma comment(lib, "gdi32.lib") 	
+#pragma comment(lib, "Opengl32.lib") 
---
+++ demo2/demo2.cpp
@@ -0,0 +1,263 @@
+﻿// demo2.cpp : 定义应用程序的入口点。
+//
+
+#include "framework.h"
+#include "demo2.h"
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_DEMO2, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DEMO2));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DEMO2));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DEMO2);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+
+
+SkPath star() {
+	const SkScalar R = 115.2f , C = 128.0f;
+	SkPath path;
+	path.moveTo(C + R, C);
+
+	for (int i = 1; i < 2; ++i) {
+		SkScalar a = 2.6927937f * i;
+		path.lineTo(C + R * cos(a), C + R * sin(a));
+	}
+	return path;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 分析菜单选择:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: 在此处添加使用 hdc 的任何绘图代码...
+
+		RECT clientRect = { 0 };
+		GetClientRect(hWnd, &clientRect);
+
+		int nWid = clientRect.right - clientRect.left;
+		int nHei = clientRect.bottom - clientRect.top;
+
+		LPVOID pBmpBits = NULL;
+
+		HBITMAP hbmp = NULL;
+		SkBitmap sbmp;
+
+
+		BITMAPINFO bmi;
+		memset(&bmi, 0, sizeof(bmi));
+		bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+		bmi.bmiHeader.biWidth = nWid;
+		bmi.bmiHeader.biHeight = -nHei; // top-down image 
+		bmi.bmiHeader.biPlanes = 1;
+		bmi.bmiHeader.biBitCount = 32;
+		bmi.bmiHeader.biCompression = BI_RGB;
+		bmi.bmiHeader.biSizeImage = 0;
+
+		//HDC hdc = GetDC(NULL);
+		//LPVOID pBits=NULL;
+		hbmp = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, &pBmpBits, 0, 0);
+		//ReleaseDC(NULL, hdc);
+
+		if (hbmp)
+		{
+			if (pBmpBits)
+			{
+				memset(pBmpBits, 0, nWid * nHei * 4);
+			}
+
+		}
+
+		sbmp.setInfo(SkImageInfo::Make(nWid, nHei, kN32_SkColorType, kPremul_SkAlphaType));
+		sbmp.setPixels(pBmpBits);
+
+
+		{
+            
+			SkCanvas* canvas = new SkCanvas(sbmp);
+			SkPaint paint;
+			paint.setPathEffect(SkDiscretePathEffect::Create(10.0f, 4.0f));
+			paint.setStyle(SkPaint::kStroke_Style);
+			paint.setStrokeWidth(2.0f);
+			paint.setAntiAlias(true);
+			paint.setColor(0xff4285F4);
+			//canvas->clear(SK_ColorWHITE);
+			SkPath path(star());
+			canvas->drawPath(path, paint);
+
+		}
+
+
+
+
+
+
+
+
+		StretchDIBits(hdc, 0, 0, nWid, nHei, 0, 0, nWid, nHei, pBmpBits, &bmi, DIB_RGB_COLORS, SRCCOPY);
+
+
+		sbmp.reset();
+		if (hbmp) DeleteObject(hbmp);
+
+		EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
